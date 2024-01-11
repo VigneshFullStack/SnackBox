cb99906f69e829f6254a3769a92ab6c747726ef9
Naveen Kumar <Naveen.Kumar@euroland.com>
2024-01-11 20:02:45 +0530

    Admin Page with route

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+import { Login, User } from 'src/models/user';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) { }
+  userDetail: Login = {} as Login;
+
+  ngOnInit() {
+    this.userDetail = JSON.parse(this.authService.getUser()!);
+    console.log("this.userDetail : ", this.userDetail);
+
+  }
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
 
 }
