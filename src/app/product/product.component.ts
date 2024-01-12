26ff913acfd0750566a044203e7ce5031023e43d
Vignesh <vignesh.fullstack7@gmail.com>
2024-01-12 19:41:19 +0530

    category card added

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -23,6 +23,9 @@ export class ProductComponent {
 
   selectedSnacks: any[] = [];
   count: number = 0;
+  showCart: boolean = false;
+
+  selectedCategory: Category | undefined;
 
   constructor(public router: Router, private snackboxService: SnackboxService, private route: ActivatedRoute) { }
 
@@ -47,72 +50,98 @@ export class ProductComponent {
     });
   }
 
+  // goToCategoryDetails(id: number): void {
+  //   this.getCategory = this.snackboxService.getCategoryById(id).subscribe((result: any) => {
+  //     console.log('getCategoryById : ', result);
+  //     this.data = result;
+
+  //     // Initialize selectedItems and selectedValues for each accordion
+  //     this.data.forEach((category: any, accordionIndex: number) => {
+  //       const itemType = category.ItemTypes[0].ItemType;
+  //       const items = category.ItemTypes[0].Items.split(',');
+  //       this.selectedItems[itemType] = [];
+  //       this.selectedItemsCount[itemType] = 0;
+  //     });
+  //   });
+  // }
+
   goToCategoryDetails(id: number): void {
     this.getCategory = this.snackboxService.getCategoryById(id).subscribe((result: any) => {
       console.log('getCategoryById : ', result);
       this.data = result;
 
-      // Initialize selectedItems and selectedValues for each accordion
+      // Reset selectedItems and selectedItemsCount for each accordion
+      this.selectedItems = {};
+      this.selectedItemsCount = {};
+
+      // Reset selected items and counts for each accordion
       this.data.forEach((category: any, accordionIndex: number) => {
         const itemType = category.ItemTypes[0].ItemType;
         const items = category.ItemTypes[0].Items.split(',');
         this.selectedItems[itemType] = [];
         this.selectedItemsCount[itemType] = 0;
+
+        // Set the selected category
+        this.selectedCategory = this.categories.find(category => category.category_id === id);
+
+        // Reset remaining items for each category
+        if (itemType === 'Snack 1' || itemType === 'Snack 2') {
+          this.remainingItems[itemType] = 3; // Adjust the default value based on your requirements
+        } else if (itemType === 'Add on' || itemType === 'Dessert') {
+          this.remainingItems[itemType] = 1; // Adjust the default value based on your requirements
+        }
+
       });
+
+      // Reset overall counts and showCart status
+      this.count = 0;
+      this.showCart = false;
+
+      // Reset remainingItems for all categories
+      // this.categories.forEach((category) => {
+      //   this.remainingItems[category.category_name] = 3;  // Adjust the default value based on your requirements
+      // });
     });
+  }
+
+  isSelected(itemType: string, item: string): boolean {
+    return this.selectedItems[itemType]?.includes(item);
   }
 
   addToCart(itemType: string, item: string) {
-    console.log('first', itemType)
     // Check if the item is already selected
-    var maxSelectionNotReached = false;
-    var wishitem = 0;
     const itemAlreadySelected = this.selectedItems[itemType].indexOf(item) !== -1;
-    if( itemType == 'Add on')    {
-      maxSelectionNotReached = this.selectedItems[itemType].length <=1;
-      wishitem = 1;
-      console.log('Add on', maxSelectionNotReached)
-    }
-    if( itemType == 'Dessert')    {
-      wishitem = 1;
-      maxSelectionNotReached = this.selectedItems[itemType].length <=1;
-      console.log('Dessert ', maxSelectionNotReached)
-    }
-    if( itemType == 'Snack 1')    {
-      wishitem = 3;
-      maxSelectionNotReached = this.selectedItems[itemType].length <=2;
-      console.log('Snack 1', maxSelectionNotReached,this.selectedItems[itemType].length)
-    }
-    if( itemType == 'Snack 2')    {
-      wishitem = 3;
-      maxSelectionNotReached = this.selectedItems[itemType].length <=2;
-      console.log('Snack 2', maxSelectionNotReached)
-    }
-    // Check if the maximum selection count has not been reached for the current itemType
-    // const maxSelectionNotReached = this.selectedItemsCount[itemType] < 3;
 
-    if (maxSelectionNotReached) {
+    // Check if the maximum selection count has not been reached for the current itemType
+    const maxSelectionNotReached =
+      (itemType === 'Snack 1') ? this.selectedItemsCount[itemType] < 3 :
+        (itemType === 'Snack 2') ? this.selectedItemsCount[itemType] < 3 :
+          (itemType === 'Add on') ? this.selectedItemsCount[itemType] < 1 :
+            (itemType === 'Dessert') ? this.selectedItemsCount[itemType] < 1 :
+              true;  // Default case
+
+    if (maxSelectionNotReached && !itemAlreadySelected) {
       this.selectedItems[itemType].push(item);
       this.selectedItemsCount[itemType]++;
-      this.SelectedItemsCount();
-      console.log('selectedItems after add: ', this.selectedItems[itemType].length);
-      console.log('this.selectedItemsCount[itemType]', this.selectedItemsCount[itemType])
+      this.count++;  // Increment the total count
     } else {
-      console.log(`Item '${item}' is already added!`);
+      console.log(`Item '${item}' is already added or max selection reached!`);
+    }
+    this.showCart = this.count > 0;
+
+
+    if (!maxSelectionNotReached) {
+      this.remainingItems[itemType] = 0;  // Set remaining items to 0
+    } else {
+      this.remainingItems[itemType] =
+        (itemType === 'Snack 1') ? 3 - this.selectedItemsCount[itemType] :
+          (itemType === 'Snack 2') ? 3 - this.selectedItemsCount[itemType] :
+            (itemType === 'Add on' || itemType === 'Dessert') ? 1 - this.selectedItemsCount[itemType] : 0;
     }
 
-    this.remainingItems[itemType] = wishitem - this.selectedItemsCount[itemType];
-    console.log('wishitem', wishitem)
-    console.log('this.remainingItems[itemType] ', this.remainingItems[itemType] )
-
-    // Enable or disable buttons based on the maximum selection status
-    this.categories.forEach((category) => {
-      const maxSelectionReached = this.selectedItemsCount[category.category_name] ===  wishitem;
-      const isEnabled = !maxSelectionReached || (category.category_name === itemType && this.selectedItems[itemType]?.includes(item));
-      this.remainingItems[category.category_name] = isEnabled ?  wishitem - (this.selectedItemsCount[category.category_name] || 0) : 0;
-    });
+    // Mark the selected item for highlighting
+    this.selectedSnacks.push({ itemType, item });
   }
-
 
   // removeItem(item: any) {
   //   this.selectedItems[item.itemType].splice(this.selectedItems[item.itemType].indexOf(item.snack_name), 1);
@@ -124,10 +153,16 @@ export class ProductComponent {
   removeItem(category: string, index: number) {
     const selectedItem = this.selectedItems[category][index];
     this.selectedItems[category].splice(index, 1);
-    this.remainingItems[category] = 3 - this.selectedItems[category].length;
-    this.SelectedItemsCount();
-    console.log('selectedItems after remove : ', this.selectedItems);
+    this.selectedItemsCount[category]--;
+    this.count--;  // Decrement the total count
+
+    this.showCart = this.count > 0;
+
+    if (this.remainingItems[category] !== undefined) {
+      this.remainingItems[category] = this.selectedItemsCount[category] < 3 ? 3 - this.selectedItemsCount[category] : 0;
+    }
   }
+
 
   removeCategory(category: string) {
     delete this.selectedItems[category];
@@ -143,12 +178,12 @@ export class ProductComponent {
     return this.count;
   }
 
-  SelectedItemsCount()  {
+  SelectedItemsCount() {
     var ItemsCount = 0;
     for (const key in this.selectedItems) {
       if (this.selectedItems.hasOwnProperty(key)) {
         this.count += this.selectedItems[key].length;
-        ItemsCount ++;
+        ItemsCount++;
       }
     }
     return ItemsCount;
